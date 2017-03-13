62720cd5981cfec5625ab30c0ab8864b7c96f420
Nikola Serdiuk <nikolay.serdiuk@propeople.com.ua>
2017-03-13 14:21:36 +0200

    Added to menu component

---
+++ components/header/header.component.spec.ts
@@ -0,0 +1,18 @@
+import {HeaderComponent} from './header.component';
+//import {provider} from '@angular/core';
+import {
+  async,
+  inject,
+  TestBed,
+} from '@angular/core/testing';
+
+describe('HeaderComponent', () => {
+  let fixture;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeaderComponent],
+      providers: []
+    })
+  });
+});
---
+++ components/menu/menu.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MenuComponent } from './menu.component';
+
+@NgModule({
+  imports: [],
+  exports: [MenuComponent],
+  declarations: [MenuComponent]
+})
+
+export class MenuModule { }
