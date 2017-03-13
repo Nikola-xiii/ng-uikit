a1a104f6dc3d333efec0091f3f912ec7b52d1a8f
Nikola Serdiuk <nikolay.serdiuk@propeople.com.ua>
2017-03-13 12:25:44 +0200

    Fixed header

--- ng-uikit.ts
+++ ng-uikit.ts
@@ -1,4 +1,3 @@
-export * from './components/select/select.module';
 export * from './components/button/button.module';
 export * from './components/header/header.module';
 export * from './components/sidebar/sidebar.module';
