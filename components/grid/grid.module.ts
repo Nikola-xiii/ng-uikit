e116425adfba1d6c1982050bead017411017a8f3
Nikola Serdiuk <nikolay.serdiuk@propeople.com.ua>
2017-03-16 13:36:52 +0200

    Added grid component basic files

---
+++ components/grid/grid.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { GridComponent } from './grid.component';
+
+@NgModule({
+  imports: [],
+  exports: [GridComponent],
+  declarations: [GridComponent]
+})
+
+export class HeaderModule { }
